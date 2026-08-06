e2bb8a545626444b40af522f83bb8a3ec5ac00d1
Praveenkumar7902 <mpraveenkumar7902@gmail.com>
2026-08-06 02:01:07 -0500

    changed the intro Description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import  ErrorBoundary  from "./ErrorBoundry";
+import ErrorBoundary from "./ErrorBoundry";
 import {
   ComputersCanvas,
   GamingPcCanvas,
@@ -16,30 +16,30 @@ const Hero = () => {
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
-        <motion.div 
-          initial={{ x : '100vw', display : "hidden"}}
+        <motion.div
+          initial={{ x: '100vw', display: "hidden" }}
           animate={{
-            x : 0,
-            display : "visible"
+            x: 0,
+            display: "visible"
           }}
           transition={{
-            delay : 3,
+            delay: 3,
             duration: 1,
-            type : "spring",
-            stiffness : 150
+            type: "spring",
+            stiffness: 150
           }}
-           className="flex flex-col justify-center items-center mt-5">
+          className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </motion.div>
-        <motion.div 
+        <motion.div
           animate={{
             scale: [1, 2, 2, 1, 1],
             rotate: [0, 0, 180, 180, 0],
           }}
           transition={{
             duration: 2,
-            when : "beforeChildren",
+            when: "beforeChildren",
             ease: "easeInOut",
             times: [0, 0.2, 0.5, 0.8, 1],
           }}
@@ -47,19 +47,19 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-cyan-500">Praveen Kumar M</span>
           </h1>
-          <motion.p 
+          <motion.p
             animate={{
-              scale : [1,1,5, 1],
+              scale: [1, 1, 5, 1],
               rotate: [0, 0, -180, -180, 0],
-              zIndex : -1 
+              zIndex: -1
             }}
             transition={{
-              delay : 2,
-              duration : 1,
+              delay: 2,
+              duration: 1,
               ease: "easeInOut",
             }}
-           className={`${styles.heroSubText} mt-2 tex-white-100`}>
-            I develop user interfaces and web applications
+            className={`${styles.heroSubText} mt-2 tex-white-100`}>
+            I'm a Full Stack Developer with 2+ years of experience building scalable, user-friendly web applications using React.js, JavaScript, Node.js, Express.js, and MongoDB.
           </motion.p>
         </motion.div>
       </div>
